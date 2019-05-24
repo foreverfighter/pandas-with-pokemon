d4e054d4f21f4973805956b9abce8ff3b073f15d
Siow Yi Sheng <siowyisheng@gmail.com>
2019-05-24 10:12:24 +0800

    WIP

---
+++ pandas_use.py
@@ -0,0 +1,6 @@
+# there are 3 main objects to know, SERIES, DATAFRAMES, NDARRAYS(ARRAYS)
+s = pd.Series([1, 2, 3])
+s = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
+
+a = np.array([1, 2, 3])
+a2d = np.array([[1, 2], [3, 4]])
