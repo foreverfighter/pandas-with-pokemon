dd05263aca11a501d719322449c1dd552532c759
Siow Yi Sheng <siowyisheng@gmail.com>
2020-01-13 07:19:05 +0800

    content: cleans up content

--- pandas_use.py
+++
@@ -1,6 +0,0 @@
-# there are 3 main objects to know, SERIES, DATAFRAMES, NDARRAYS(ARRAYS)
-s = pd.Series([1, 2, 3])
-s = pd.Series([1, 2, 3], index=['a', 'b', 'c'])
-
-a = np.array([1, 2, 3])
-a2d = np.array([[1, 2], [3, 4]])
